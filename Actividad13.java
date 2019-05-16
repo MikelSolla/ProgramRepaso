14099a7d643a2425c1bf7e188050bdfacc19ecdd
Mikel <mikelsolla007@gmail.com>
2019-05-16 18:40:07 +0200

    ejercicio13

--- Actividad13.java
+++ Actividad13.java
@@ -19,6 +19,7 @@ public class Actividad13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*me sale siempre el mensaje "direccion mal escrita"*/
         String calle = JOptionPane.showInputDialog("Introduce la calle en la que vives(calle, numero, piso, letra(Mayus.), ciudad, codigo postal)");
         Pattern patron = Pattern.compile("^calle [A-Z]*[a-z]+ [A-Z]*[a-z]* [A-Z]*[a-z]* [A-Z]*[a-z]* ,[0-9]{1,100},[0-9]{1,20},[A-J],[A-Z]*[a-z]+,[0-9]{5}");
         Matcher encaja = patron.matcher(calle);
