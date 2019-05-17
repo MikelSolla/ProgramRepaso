c163c7c336eea1e200dd492c989fdd4b705cc88a
Unknown <mikelsolla007@gmail.com>
2019-05-17 14:06:26 +0200

    cambios

--- Actividad14/src/actividad14/Actividad14.java
+++ Actividad14/src/actividad14/Actividad14.java
@@ -1,5 +1,8 @@
 package actividad14;
 
+import java.util.ArrayList;
+import Clases.Persona;
+import javax.swing.JOptionPane;
 /**
  *
  * @author 1gdaw08
@@ -10,6 +13,17 @@ public class Actividad14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ArrayList <Persona> persona;
+        persona = new ArrayList();
+        
+        String dni = JOptionPane.showInputDialog("Introduce el DNI");
+        String nombre = JOptionPane.showInputDialog("Introduce el nombre");
+        String apellido = JOptionPane.showInputDialog("Introduce el apellido");
+        String sexo = JOptionPane.showInputDialog("Introduce el sexo");
+        String edad = JOptionPane.showInputDialog("Introduce el edad");
+        String peso = JOptionPane.showInputDialog("Introduce el peso");
+        
+        
         
     }
     
