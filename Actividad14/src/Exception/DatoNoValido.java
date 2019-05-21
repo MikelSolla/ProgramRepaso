ea9f16462013b49075f39c2c9302d306595c4c6b
Unknown <mikelsolla007@gmail.com>
2019-05-21 10:56:58 +0200

    validaciones

---
+++ Actividad14/src/Exception/DatoNoValido.java
@@ -0,0 +1,5 @@
+package Exception;
+
+public class DatoNoValido extends Exception{
+   
+}
--- Actividad14/src/actividad14/Actividad14.java
+++ Actividad14/src/actividad14/Actividad14.java
@@ -1,5 +1,6 @@
 package actividad14;
 
+import Exception.*;
 import java.util.ArrayList;
 import Clases.Persona;
 import javax.swing.JOptionPane;
@@ -12,17 +13,23 @@ public class Actividad14 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         
         ArrayList<Persona>lista=new ArrayList();
          do{
             String dni = JOptionPane.showInputDialog("Teclea el DNI");
+            validarDNI(dni);
             String nombre = JOptionPane.showInputDialog("Teclea el nombre");
+            validarNombreApellido(nombre);
             String apellido = JOptionPane.showInputDialog("Teclea el apellido");
-            String sexo = JOptionPane.showInputDialog("Teclea el sexo");
+            validarNombreApellido(apellido);
+            String sexo = JOptionPane.showInputDialog("Teclea el sexo(H/M)");
+            validarSexo(sexo);
             String edad = JOptionPane.showInputDialog("Teclea la edad");
+            validarEdad(edad);
             String peso = JOptionPane.showInputDialog("Teclea el peso");
+            validarPeso(peso);
                  
             Persona p= new Persona(dni,nombre,apellido,sexo,edad,peso);
                  
@@ -37,7 +44,7 @@ public class Actividad14 {
             case 1:
                 String dni =JOptionPane.showInputDialog("Introduce el DNI que quieres buscar");
                 int x;
-                for(x = 0; x < lista.size() && lista.get(x).getDNI().equals(dni)== false; x++){}
+                for(x = 0; x < lista.size() && lista.get(x).getDNI().equals(dni)== false; x++)
                 if( x == lista.size())
                     JOptionPane.showMessageDialog(null,"Persona con ese DNI no encontrada");
                 else
@@ -47,7 +54,7 @@ public class Actividad14 {
             case 2:
                 String nombre =JOptionPane.showInputDialog("Introduce la persona que quieres borrar");
                 int y;
-                for(y = 0; y < lista.size() && lista.get(y).getNombre().equals(nombre)== false; y++){}
+                for(y = 0; y < lista.size() && lista.get(y).getNombre().equals(nombre)== false; y++)
                 if( y == lista.size())
                     JOptionPane.showMessageDialog(null,nombre+" no encontrado/a");
                 else
@@ -60,10 +67,10 @@ public class Actividad14 {
                     JOptionPane.showMessageDialog(null, nombre+" ha sido borrado");
                 break;
             case 3:
-                /*Ordenar por apellido*/
+                /*no se como hacerlo*/
                 break;
             case 4:
-                /*Mostrar todas las personas*/
+                
                 break;
             case 5:
                 System.exit(0);
@@ -73,6 +80,47 @@ public class Actividad14 {
         while(menu !=5);
         
      
+    }
+    public static void validarNombreApellido(String nombre)throws Exception{
+        
+        boolean error=false;
+        if (nombre.length()<= 2)
+            error = true;
+        else
+            // Todo letras
+            for (int x = 0; x < nombre.length() && error == false;x++)
+                if (Character.isDigit(nombre.charAt(x)))
+                    error = true;
+        if (error)
+            throw new DatoNoValido();
+                    
+    }
+    public static void validarSexo(String sexo)throws Exception{
+        boolean error = false;
+        if(!"H".equals(sexo) && !"M".equals(sexo))
+            error =true;
+        if (error)
+            throw new DatoNoValido();
+    }
+    public static void validarEdad(String edad)throws Exception{
+        boolean error = false;
+        if(Integer.parseInt(edad)<0 || Integer.parseInt(edad)>150)
+            error=true;
+        if (error)
+            throw new DatoNoValido();
+    }
+    public static void validarPeso(String peso)throws Exception{
+        boolean error = false;
+        if(Integer.parseInt(peso)<35 || Integer.parseInt(peso)>150)
+            error=true;
+        
+        if (error)
+            throw new DatoNoValido();
+    }
+    public static void validarDNI(String dni)throws Exception{
+        boolean error=false;
+        if (dni.length()== 9)
+            error = true;
     }
  }
     
